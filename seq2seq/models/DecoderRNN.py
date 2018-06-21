a6cf12c7e4aea21c3af8644922e36a232044b1c9
vdankers <verna.dankers@student.uva.nl>
2018-06-21 13:20:19 +0200

    Added clean memory for attention module

--- seq2seq/models/DecoderRNN.py
+++ seq2seq/models/DecoderRNN.py
@@ -153,7 +153,7 @@ class DecoderRNN(BaseRNN):
 
     def forward(self, inputs=None, encoder_hidden=None, encoder_outputs=None,
                     function=F.log_softmax, teacher_forcing_ratio=0, provided_attention=None):
-
+        self.attention.clean_memory()
         ret_dict = dict()
         if self.use_attention:
             ret_dict[DecoderRNN.KEY_ATTN_SCORE] = list()
